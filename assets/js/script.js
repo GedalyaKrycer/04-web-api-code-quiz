d253b2903c3a956e7c8ea3d5753400055fa8e0da
Gedalya Krycer <gedalya@krycer.com>
2020-04-24 20:12:24 -0700

    Added JS object for all answers and variable to target main content section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,43 @@
-console.log("Hello world");
+// Variable For DOM Elements
+let mainContentSect = document.querySelector("#content");
+
+// Object that holds all the answers  
+let answers = {
+    question1: {
+        answer1: false,
+        answer2: false,
+        answer3: false,
+        answer4: false
+    },
+    question2: {
+        answer1: false,
+        answer2: false,
+        answer3: false,
+        answer4: false
+    },
+    question3: {
+        answer1: false,
+        answer2: false,
+        answer3: false,
+        answer4: false
+    },
+    question4: {
+        answer1: false,
+        answer2: false,
+        answer3: false,
+        answer4: false
+    },
+    question5: {
+        answer1: false,
+        answer2: false,
+        answer3: false,
+        answer4: false
+    }
+};
+
+// console.log(answers.question1.answer1);
+
+
+function buttonPress() {
+
+}
