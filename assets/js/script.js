09d70ae639902d976118014196e1aa71f838e4d0
Gedalya Krycer <gedalya@krycer.com>
2020-04-24 21:22:21 -0700

    Added startScreen() function to show question 1.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-// Variable For DOM Elements
+// Global Scope Variables For DOM Elements
 let mainContentSect = document.querySelectorAll("div");
-let startQuizBtn = document.querySelector("#startQuiz");
 
 
 // Object that holds all the answers  
@@ -37,15 +36,26 @@ let answers = {
     }
 };
 
-// console.log(answers.question1.answer1);
 
+
+// Event Listener For Button Clicks
 for (let i = 0; i < mainContentSect.length; i++) {
     mainContentSect[i].addEventListener("click", function(event) {
-        if (event.target.type == "button" && event.target === startQuizBtn) {
-            console.log("Button start");
-        }
+        startScreen();
         
     });
 };
+
+
+// This starts the quiz
+function startScreen() {
+    const startQuizBtn = document.querySelector("#startQuiz");
+    const startQuizDisplay = document.querySelector(".start-content");
+    
+    if (event.target.type == "button" && event.target === startQuizBtn) {
+        console.log("Button start");
+        startQuizDisplay.style.display = "none";
+    }
+}
  
 
